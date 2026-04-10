2c3895e3e5132c3f78ce9a68c6871945dfe9e920
aluyapeter <aluyapeter7@gmail.com>
2026-04-10 12:49:06 +0100

    implemented test db for pytest insatnce

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,44 @@
+import pytest
+import os
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from main import app
+from database import get_db
+from models import Base
+
+load_dotenv()
+
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+
+if TEST_DATABASE_URL is None:
+    raise ValueError("No test db url")
+
+engine = create_engine(TEST_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown_db():
+    """
+    Because autouse=True, Pytest will run this before EVERY single test function.
+    """
+    Base.metadata.create_all(bind=engine)
+    
+    yield
+    
+    Base.metadata.drop_all(bind=engine)
+
+def override_get_db():
+    """This function behaves exactly like your normal get_db, but uses the test session."""
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
